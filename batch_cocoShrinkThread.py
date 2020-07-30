364b21f3786b5c56dd890630e34d8efd35df391c
Cangyu Lin <jefflin.je598@gmail.com>
2020-07-30 23:03:27 +0800

    beautifying process added

--- batch_cocoShrinkThread.py
+++ batch_cocoShrinkThread.py
@@ -49,7 +49,7 @@ class batch_sncThread(QtCore.QThread):
     progressBar = QtCore.pyqtSignal(int)
     progressBar_setMaximum = QtCore.pyqtSignal(int)
     EOF_STATUS = False
-    
+
     def run_func(self,zips,filenames,json_name,folder):
         def parseInt(a):
             filenum=""
@@ -65,7 +65,7 @@ class batch_sncThread(QtCore.QThread):
                 filenum="0000"
             return filenum
         count2 = 0
-                   
+
         # Sorting
         zips.sort()
         filenames.sort()
@@ -74,11 +74,11 @@ class batch_sncThread(QtCore.QThread):
 
         for j in range(len(zips)):
             for i in range(len(filenames)):
-                
+
                 count2+=1
                 # declare ROI file
                 roi = read_roi.read_roi_zip(self.coco_path+"/"+zips[j])
-                roi_list = list(roi.values())  
+                roi_list = list(roi.values())
                 # ROI related file informations
                 filename = filenames[i].replace("./", "")
                 im = cv2.imread(self.coco_path+'/'+filename)
@@ -144,13 +144,14 @@ class batch_sncThread(QtCore.QThread):
                                     y_list[k] = h-1
 
                             # os.remove(self.coco_path+"/"+zips[j])
+                            # self.append_coco.emit("removed"+str(self.coco_path+"/"+zips[j]))
                             # roi_obj = ROIPolygon(x_list, y_list)
                             # with ROIEncoder(parseInt(j+1)+"-"+parseInt(file_sum)+"-0000"+".roi", roi_obj) as roi:
                             #     roi.write()
                             # with ZipFile(self.coco_path+"/"+zips[j], 'a') as myzip:
-                            #     myzip.write(parseInt(j+1)+"-"+parseInt(file_sum)+"-0000"+".roi")
-                            #     self.append_coco.emit("Compressed "+parseInt(j+1)+"-"+parseInt(file_sum)+"-0000"+".roi")
-                            # os.remove(parseInt(j+1)+"-"+parseInt(file_sum)+"-0000"+".roi")
+                            #     myzip.write(parseInt(i+1)+"-"+parseInt(file_sum)+"-0000"+".roi")
+                            #     self.append_coco.emit("Shrinked "+parseInt(j+1)+"-"+parseInt(file_sum)+"-0000"+".roi")
+                            # os.remove(parseInt(i+1)+"-"+parseInt(file_sum)+"-0000"+".roi")
 
                             # parameters
                             # x_list.append(int(a["x"][0]/4))
@@ -218,7 +219,7 @@ class batch_sncThread(QtCore.QThread):
                                     "region_attributes": {
                                         "name": dirname(folder)
                                     }
-                                } 
+                                }
                             }
                             data[filename + str(size)]["regions"].update(regions)
                             original.update(data)
@@ -244,26 +245,30 @@ class batch_sncThread(QtCore.QThread):
                                     },
                                     "region_attributes": {"name": dirname(folder)},
                                 }
-                            
+
                             }
                             data[filename + str(size)]["regions"].update(regions)
-                            original.update(data)
+                            try:
+                                original.update(data)
+                            except ValueError:
+                                print("Decode error, passed")
                     except IndexError or FileNotFoundError:
-                        self.append_coco.emit("[ERROR] Can't find any type specific files! (Maybe check the file type)")        
+                        self.append_coco.emit("[ERROR] Can't find any type specific files! (Maybe check the file type)")
+                    
                 with io.open(json_name, "w", encoding="utf-8") as f:
-                    f.write(json.dumps(original, ensure_ascii=False))
+                    f.write(json.dumps(original, sort_keys=True, indent=4, ensure_ascii=False))
                     f.close()
         print("Converted File: ", json_name)
-        
-            
-        
+
+
+
     def run(self):
-        
+
         os.chdir(self.coco_path)
         #self.append_coco.emit("Current Path: "+self.coco_path)
         path ="."
         # ROI arrays
-        dirs =[]    
+        dirs =[]
         #scanning
         count =0
         for d in os.walk(path):
@@ -274,25 +279,25 @@ class batch_sncThread(QtCore.QThread):
                             count+=1
                         elif ".zip" in file:
                             count+=1
-        
+
         #print(count)
         i=1
         j=1
         procs=[]
         for d in os.walk(path):
             for folder in d[1]:
-                
+
                 for r,d,f in os.walk(str(folder)):
                     filenames = []
                     zips = []
                     for file in f:
-                        
+
                         if os.path.splitext(file)[-1] == self.txt:
                             filenames.append(os.path.join(r, file))
                         elif os.path.splitext(file)[-1] == ".zip":
                             zips.append(os.path.join(r, file))
-                    
-                    p = Process(target=self.run_func, args=(zips,filenames,"via_region_data_part_"+str(i)+".json",folder))                    
+
+                    p = Process(target=self.run_func, args=(zips,filenames,"via_region_data_part_"+str(i)+".json",folder))
                     procs.append(p)
                     i+=1
         self.append_coco.emit("Scanning completed! i="+str(i))
@@ -312,22 +317,25 @@ class batch_sncThread(QtCore.QThread):
         self.append_coco.emit("Combining...")
         for f in glob.glob("*.json"):
             with open(f, "r") as infile:
-                result.update(json.load(infile))
+                try:
+                    result.update(json.load(infile))
+                except ValueError:
+                    print("Decode error, passed")
                 infile.close()
         with open("via_region_data.json", "w") as outfile:
-             json.dump(result, outfile)
+             json.dump(result, outfile, sort_keys=True, indent=4)
              outfile.close()
         self.progressBar.emit(i)
         self.append_coco.emit("---CONVERT ENDED----")
         self.append_coco.emit("---" + str(time.time() - start_time)+"secs ----")
-                
-                
-        
-                    
-        
-        
-                        
-        
-                        
-        
+
+
+
+
+
+
+
+
+
+
 
