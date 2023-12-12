062cce95fe2775f0f01b336316182d8c21126b84
Cangyu Lin <jefflin.je598@gmail.com>
2023-12-12 13:40:46 +0800

    Add files via upload

--- trainingThread.py
+++ trainingThread.py
@@ -51,6 +51,73 @@ sys.setrecursionlimit(5000)  # Set a higher recursion limit
 import signal
 
 signal.signal(signal.SIGTERM, signal.SIG_DFL)
+from mrcnn.utils import Dataset, compute_ap
+
+############################################################
+#  Custom Callbacks
+############################################################
+from tensorflow.keras.callbacks import Callback
+class MeanAveragePrecisionCallback(Callback):
+    def __init__(self, train_model: modellib.MaskRCNN, inference_model: modellib.MaskRCNN, dataset: Dataset,
+                 calculate_map_at_every_X_epoch=1, dataset_limit=None,
+                 verbose=1):
+        super().__init__()
+        self.train_model = train_model
+        self.inference_model = inference_model
+        self.dataset = dataset
+        self.calculate_map_at_every_X_epoch = calculate_map_at_every_X_epoch
+        self.dataset_limit = len(self.dataset.image_ids)
+        if dataset_limit is not None:
+            self.dataset_limit = dataset_limit
+        self.dataset_image_ids = self.dataset.image_ids.copy()
+
+        if inference_model.config.BATCH_SIZE != 1:
+            raise ValueError("This callback only works with the bacth size of 1")
+
+        self._verbose_print = print if verbose > 0 else lambda *a, **k: None
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        if epoch > 2 and (epoch+1)%self.calculate_map_at_every_X_epoch == 0:
+            self._verbose_print("Calculating mAP...")
+            self._load_weights_for_model()
+
+            mAPs = self._calculate_mean_average_precision()
+            mAP = np.mean(mAPs)
+
+            if logs is not None:
+                logs["val_mean_average_precision"] = mAP
+
+            self._verbose_print("mAP at epoch {0} is: {1}".format(epoch+1, mAP))
+
+        super().on_epoch_end(epoch, logs)
+
+    def _load_weights_for_model(self):
+        last_weights_path = self.train_model.find_last()
+        self._verbose_print("Loaded weights for the inference model (last checkpoint of the train model): {0}".format(
+            last_weights_path))
+        self.inference_model.load_weights(last_weights_path,
+                                          by_name=True)
+
+    def _calculate_mean_average_precision(self):
+        mAPs = []
+
+        # Use a random subset of the data when a limit is defined
+        np.random.shuffle(self.dataset_image_ids)
+
+        for image_id in self.dataset_image_ids[:self.dataset_limit]:
+            image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(self.dataset, self.inference_model.config,
+                                                                             image_id, use_mini_mask=False)
+            molded_images = np.expand_dims(modellib.mold_image(image, self.inference_model.config), 0)
+            results = self.inference_model.detect(molded_images, verbose=0)
+            r = results[0]
+            # Compute mAP - VOC uses IoU 0.5
+            AP, _, _, _ = utils.compute_ap(gt_bbox, gt_class_id, gt_mask, r["rois"],
+                                           r["class_ids"], r["scores"], r['masks'])
+            mAPs.append(AP)
+
+        return np.array(mAPs)
+
 
 # Limit GPU memory growth
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -141,7 +208,7 @@ class trainingThread(QtCore.QThread):
             """Configuration for training on the toy  dataset.
             Derives from the base Config class and overrides some values.
             """
-            MAX_GT_INSTANCES = 1
+            MAX_GT_INSTANCES = 100
             IMAGE_RESIZE_MODE = "square"
             # Give the configuration a recognizable name
             NAME = "cell"
@@ -156,7 +223,7 @@ class trainingThread(QtCore.QThread):
             # NUM_CLASSES = 1 + 1 # Background + cell
             # Number of training steps per epoch
             STEPS_PER_EPOCH = self.epoches
-            IMAGE_MAX_DIM = 64
+            IMAGE_MAX_DIM = 256
             IMAGE_MIN_DIM = 64
             # Backbone network architecture
             BACKBONE = "resnet101"
@@ -182,9 +249,13 @@ class trainingThread(QtCore.QThread):
             # Validation dataset
             dataset_val = CustomDataset()
             dataset_val.load_custom(self.dataset_path, "val")
-
+            print("Loading testing dataset")
             dataset_val.prepare()
-            
+            # Validation dataset
+            dataset_test = CustomDataset()
+            dataset_test.load_custom(self.dataset_path, "test")
+
+            dataset_test.prepare()
             # *** This training schedule is an example. Update to your needs ***
             # Since we're using a very small dataset, and starting from
             # COCO trained weights, we don't need to train too long. Also,
@@ -206,17 +277,16 @@ class trainingThread(QtCore.QThread):
                         ]))
             # tf.compat.v1.enable_eager_execution()
             # add callback to calculate the result of accuracy
-            
-            # mean_average_precision_callback = MeanAveragePrecisionCallback(model,
-            #   model_inference, dataset_val, calculate_map_at_every_X_epoch=1, verbose=1, dataset_limit=100)
-            
+
+            mean_average_precision_callback = MeanAveragePrecisionCallback(model, model_inference, dataset_test, calculate_map_at_every_X_epoch=1, verbose=1, dataset_limit=100)
+
             self.update_training_status.emit("Training network heads")
             
             model.train(dataset_train, dataset_val,
                     learning_rate=config.LEARNING_RATE,
                     epochs=int(self.steps),
                     layers='heads',
-                    # custom_callbacks=[mean_average_precision_callback],
+                    custom_callbacks=[mean_average_precision_callback],
                     augmentation = aug,
                     )
            
